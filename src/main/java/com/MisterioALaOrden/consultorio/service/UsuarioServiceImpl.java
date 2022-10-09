fdc2ff5617e595e9ed5d16e2fffc709dbca80f88
Sergio Velez <svleez@gmail.com>
2022-10-08 19:39:58 -0500

    Creacion usuario service impl

---
+++ src/main/java/com/MisterioALaOrden/consultorio/service/UsuarioServiceImpl.java
@@ -0,0 +1,45 @@
+package com.MisterioALaOrden.consultorio.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.MisterioALaOrden.consultorio.model.Usuario;
+import com.MisterioALaOrden.consultorio.repository.UsuarioRepository;
+
+public class UsuarioServiceImpl implements UsuarioService{
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Override
+	@Transactional
+	public List<Usuario> consultarUsuarios() {
+		List<Usuario> listaUsuarios = this.usuarioRepository.findAll();
+		return listaUsuarios;
+	}
+
+	@Override
+	@Transactional
+	public Optional<Usuario> consultarUsuarioPorId(Integer id) {
+		Optional<Usuario> respUsuario = this.usuarioRepository.findById(id);
+		return respUsuario;
+	}
+
+	@Override
+	@Transactional
+	public Usuario guardarUsuario(Usuario usuario) {
+		Usuario newUsuario = this.usuarioRepository.save(usuario);
+		return newUsuario;
+	}
+
+	@Override
+	@Transactional
+	public void eliminarUsuario(Integer id) {
+		this.usuarioRepository.deleteById(id);
+	}
+
+}
