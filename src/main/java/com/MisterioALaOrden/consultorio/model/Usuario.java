4f9a556c86b166e0d6f9443fc2c88addfa6a3c04
Sergio Velez <svleez@gmail.com>
2022-10-08 19:03:37 -0500

    Adicion getters y setters

--- src/main/java/com/MisterioALaOrden/consultorio/model/Usuario.java
+++ src/main/java/com/MisterioALaOrden/consultorio/model/Usuario.java
@@ -1,5 +1,6 @@
 package com.MisterioALaOrden.consultorio.model;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Column;
@@ -11,7 +12,12 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "usuario")
-public class Usuario{
+public class Usuario implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2083916305807004865L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,4 +34,61 @@ public class Usuario{
 	
 	@Column(name = "estado", length = 45)
 	private String estado;
+
+	
+	public Usuario() {
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public Date getFechaHoraCreacion() {
+		return fechaHoraCreacion;
+	}
+
+
+	public void setFechaHoraCreacion(Date fechaHoraCreacion) {
+		this.fechaHoraCreacion = fechaHoraCreacion;
+	}
+
+
+	public String getTipo() {
+		return tipo;
+	}
+
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+
+	public String getEstado() {
+		return estado;
+	}
+
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	
+	
 }
